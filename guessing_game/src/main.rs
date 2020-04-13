1c04ecd91b0910ccd828baf5f6e4518e499fceb9
Gianfranco Dutka <gdutk007@gmail.com>
2020-04-13 17:36:21 -0400

    working on guessing game. Still needs to add error handling

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,35 @@
+
+use std::io;
+use rand::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+
+    // generating random number
+    let mut rng = rand::thread_rng();
+    let x : u32 = rng.gen_range(1,100);
+
+    let mut guess = String::new();
+    // references are immutable by default, need to add '& mut' or we wont be able to 
+    // modify memory
+    println!("Guess a Number!");
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to Read Line!");
+
+   let mut number: u32 = guess.trim().parse().unwrap();
+
+   let mut done = false;
+    while !done {
+        println!("You Guessed {} ", guess.trim());
+        println!("The Secret Number is : {}", x);   
+        if number == x {
+            done  = true;
+            println!("Correct! You guessed the secret Number!");
+        }else{
+            println!("Incorrect, Guess Another Number!");
+            let mut guess = String::new();
+            io::stdin().read_line(&mut guess)
+                .expect("Crashed reading user Input!");
+            number = guess.trim().parse().unwrap();
+        }
+    }
 }
