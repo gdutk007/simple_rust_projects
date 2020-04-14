922147b1d58b72f5a928dbbaa23006eef783b4c1
Gianfranco Dutka <gdutk007@gmail.com>
2020-04-13 20:35:28 -0400

    Got loop working with error handling using the 'match' keyword. Also used the 'match' keyword with 'std::cmp::Ordering' for better readability.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,7 @@
 
 use std::io;
 use rand::prelude::*;
+use std::cmp::Ordering;
 
 fn main() {
 
@@ -8,28 +9,30 @@ fn main() {
     let mut rng = rand::thread_rng();
     let x : u32 = rng.gen_range(1,100);
 
-    let mut guess = String::new();
-    // references are immutable by default, need to add '& mut' or we wont be able to 
-    // modify memory
-    println!("Guess a Number!");
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to Read Line!");
-
-   let mut number: u32 = guess.trim().parse().unwrap();
-
-   let mut done = false;
-    while !done {
-        println!("You Guessed {} ", guess.trim());
-        println!("The Secret Number is : {}", x);   
-        if number == x {
-            done  = true;
-            println!("Correct! You guessed the secret Number!");
-        }else{
-            println!("Incorrect, Guess Another Number!");
-            let mut guess = String::new();
-            io::stdin().read_line(&mut guess)
-                .expect("Crashed reading user Input!");
-            number = guess.trim().parse().unwrap();
+ 
+    loop {
+        let mut guess = String::new();
+        
+        // references are immutable by default, need to add '& mut' or we wont be able to 
+        // modify memory
+       
+        println!("Guess a Number!");
+        
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to Read Line!");
+        let guess : u32 = match  guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("The secret number is {}", x);
+        match guess.cmp(&x) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!(" You Win, Congrats! ");
+                break;
+            }
         }
-    }
+
+   }
 }
